8e4bbc68233a530eb33c7fa756df4ae6d6ed73fe
Greg <gdsuaperoii@iskolarngbayan.pup.edu.ph>
2023-06-21 05:53:28 +0800

    preserve spaces and line breaks for messages

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,9 +1,9 @@
-import { cardVariants, Color } from '@/components';
-import { db } from '@/firebase';
-import clsx from 'clsx';
-import { doc, getDoc } from 'firebase/firestore';
-import dayjs, { dateFormat } from '@/lib/dayjs';
-import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
+import { cardVariants, Color } from "@/components";
+import { db } from "@/firebase";
+import clsx from "clsx";
+import { doc, getDoc } from "firebase/firestore";
+import dayjs, { dateFormat } from "@/lib/dayjs";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 
 export default function Post({
   data,
@@ -14,7 +14,7 @@ export default function Post({
     <div className="grid grid-cols-1 w-full md:w-1/2 mx-auto">
       <div
         className={clsx(
-          'w-full mx-auto mb-2 p-8 md:p-16 text-gray-100 rounded-md',
+          "w-full mx-auto mb-2 p-8 md:p-16 text-gray-100 rounded-md",
           cardVariants[color as Color]
         )}
       >
@@ -22,7 +22,9 @@ export default function Post({
           <span className="text-gray-200">To: </span>
           {to}
         </h2>
-        <p className="text-gray-200 text-md md:text-xl">{message}</p>
+        <p className="text-gray-200 text-md md:text-xl">
+          <pre className="whitespace-pre-wrap font-sans">{message}</pre>
+        </p>
       </div>
       <div className="flex items-center justify-between font-semibold text-gray-400 text-sm md:text-lg">
         <span>{badge}</span>
@@ -34,7 +36,7 @@ export default function Post({
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   const id = params?.id as string;
-  const response = await getDoc(doc(db, 'posts', id));
+  const response = await getDoc(doc(db, "posts", id));
   if (!response.data()) {
     return { notFound: true };
   }
